5c88c0068ea97271bfe8e99a6bb99bc66b52eddc
Massan0917 <massan000917@gmail.com>
2025-03-20 11:50:54 +0900

    カルーセル画面の仮作成

---
+++ frontend/app/src/components/Carousel.vue
@@ -0,0 +1,45 @@
+<template>
+  <Carousel v-bind="config">
+    <Slide v-for="image in images" :key="image.id">
+        <img :src="image.url" alt="image" />
+    </Slide>
+
+    <template #addons>
+      <Navigation />
+    </template>
+  </Carousel>
+</template>
+
+<script setup>
+import 'vue3-carousel/carousel.css'
+import { Carousel, Slide, Navigation } from 'vue3-carousel'
+
+const images = Array.from({ length: 10 }, (_, index) => ({
+  id: index + 1,
+  url: `https://picsum.photos/seed/${Math.random()}/800/600`,
+}))
+
+const config = {
+    autoplay: 1000,
+    height: 200,
+    itemsToShow: 2,
+    gap: 5,
+    wrapAround: true,
+}
+</script>
+
+
+<style>
+
+.carousel {
+  --vc-nav-background: rgba(255, 255, 255, 0.7);
+  --vc-nav-border-radius: 100%;
+}
+
+img {
+  border-radius: 8px;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+</style>
--- frontend/app/src/router.js
+++ frontend/app/src/router.js
@@ -1,10 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import CreatePost from './components/CreatePost.vue';
 import ViewPost from './components/ViewPost.vue';
+import Carousel from './components/Carousel.vue';
 
 const routes = [
   { path: '/', component: CreatePost },
-  { path: '/view', component: ViewPost }
+  { path: '/view', component: Carousel }
 ];
 
 const router = createRouter({
