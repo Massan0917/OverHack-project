31699830ae0d50ab3d11f7364388aa13cc1a1938
Takuto K <mtmtakutom8627@gmail.com>
2025-03-20 17:21:02 +0900

    投稿完了画面の作成

---
+++ frontend/app/src/components/CompletePost.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="max-w-md mx-auto p-4 text-center">
+      <h1 class="text-xl font-bold mb-4">投稿されました！</h1>
+  
+      <p class="mb-6">画面に反映されているか見てみよう！</p>
+  
+      <!-- インスタ投稿画面への遷移（仮リンク） -->
+      <a href="https://www.instagram.com/" target="_blank" class="inline-block mb-6">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Instagram_logo_2016.svg"
+          alt="Instagramへのリンク"
+          class="mx-auto w-16 h-16"
+        >
+      </a>
+  
+      <!-- 最初の画面に戻るボタン -->
+      <button @click="goToTop" class="border py-2 px-4 rounded">
+        最初の画面に戻る
+      </button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      goToTop() {
+        this.$router.push('/');
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  /* 必要に応じて追加スタイルを記述 */
+  </style>
+  
--- frontend/app/src/router.js
+++ frontend/app/src/router.js
@@ -3,6 +3,7 @@ import CreatePost from './components/CreatePost.vue';
 import ViewPost from './components/ViewPost.vue';
 import Carousel from './components/Carousel.vue';
 import ConfirmPost from './components/ConfirmPost.vue';
+import CompletePost from './components/CompletePost.vue';
 
 const routes = [
   { path: '/', name:'CreatePost', component: CreatePost },
@@ -16,6 +17,7 @@ const routes = [
       comment : route.params.comment,
     })
   },
+  { path:'/complete', name:'PostComplete', component: CompletePost}
 ];
 
 const router = createRouter({
