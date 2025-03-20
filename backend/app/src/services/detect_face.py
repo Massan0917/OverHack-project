ad52ef026142c127779f2d40f90f641bc558e774
ShimaShima0321 <03ymt.hrk.kmt@gmail.com>
2025-03-20 16:16:44 +0900

    opencv2

--- backend/app/src/services/detect_face.py
+++ backend/app/src/services/detect_face.py
@@ -2,9 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as img
 from src.models.bounding_box import BoundingBox
-import requests
 import urllib
-from io import BytesIO
+import cv2
 
 
 
@@ -14,40 +13,46 @@ def detect_faces(
     input_path: str,
     bounding_boxes: list[BoundingBox],
 ) -> bool:
-    # 画像を読み込み
-    url = urllib.parse.unquote(input_path)
-    response = requests.get(url)
-    file_contents = BytesIO(response.content)
-    image = img.imread(file_contents, format="jpg").copy()
 
+    # 画像を読み込む
+    url = urllib.parse.unquote(input_path)
+    response = urllib.request.urlopen(url)
+    image_array = np.asarray(bytearray(response.read()), dtype=np.uint8)
+    image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+
+    # スタンプ画像を読み込む
+    stamp = cv2.imread("assets/images/stamp_test.png", cv2.IMREAD_UNCHANGED)
+
+    # 合成（例: 中心(200, 150)、幅100、高さ80）
     for box in bounding_boxes:
-        __detect_one_face(image, box)
+        image = __detect_one_face(image, stamp, box)
 
     # 画像を保存
     output_path = f"static/masked/{file_name}.jpg"
-    plt.imsave(f"{output_path}", image)
+    cv2.imwrite(output_path, image)
 
     output_url = "http://localhost:3000/" + output_path
-
     return output_url
 
-def __detect_one_face(image: np.ndarray, box: BoundingBox) -> np.ndarray:
-    # TODO
 
-    color = (1,0,0)
 
-    if image.dtype == np.uint8:
-        color = tuple(int(c * 255) for c in color)  # 0-255 に変換
-    elif image.dtype == np.float32 or image.dtype == np.float64:
-        color = tuple(c for c in color)  # そのまま使用
+def __detect_one_face(image, stamp, box: BoundingBox):
+    # 画像を BGR に変換（透過情報がない場合は無視）
+    if stamp.shape[2] == 4:
+        stamp = cv2.cvtColor(stamp, cv2.COLOR_BGRA2BGR)
 
-    x, y, w, h = box.x_center, box.y_center, box.width, box.height
+    # 画像をリサイズ
+    stamp = cv2.resize(stamp, (box.width, box.height))
 
-    # 塗りつぶす領域の開始・終了座標を計算
-    x1, x2 = max(0, x - w // 2), min(image.shape[1], x + w // 2)
-    y1, y2 = max(0, y - h // 2), min(image.shape[0], y + h // 2)
+    # 貼り付ける領域の座標を計算
+    x1, y1 = box.x_center - box.width // 2, box.y_center - box.height // 2
+    x2, y2 = x1 + box.width, y1 + box.height
 
-    # 指定領域を塗りつぶし
-    image[y1:y2, x1:x2, :3] = color
+    # 範囲を超えないように制限
+    x1, y1 = max(0, x1), max(0, y1)
+    x2, y2 = min(image.shape[1], x2), min(image.shape[0], y2)
+
+    # 背景画像の該当部分を前景画像に置き換え
+    image[y1:y2, x1:x2] = stamp[:y2-y1, :x2-x1]
 
     return image
