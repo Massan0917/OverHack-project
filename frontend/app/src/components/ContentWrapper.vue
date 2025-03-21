c7aea61fc8103194c064fd61f8550a4477a2dd2c
Takuto K <mtmtakutom8627@gmail.com>
2025-03-21 23:11:37 +0900

    使いまわせるデザインの外枠のコンポーネント

---
+++ frontend/app/src/components/ContentWrapper.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="max-w-md mx-auto p-6 bg-white bg-opacity-80 rounded-lg shadow-lg">
+      <h2 class="text-3xl font-extrabold text-red-600 mb-2 text-center font-serif">Masquerade Panel</h2>
+      <slot></slot>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "ContentWrapper",
+  };
+  </script>
+  
+  <style scoped>
+  /* 必要に応じて追加スタイル */
+  </style>
+  
